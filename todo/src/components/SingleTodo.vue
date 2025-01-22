dd463236c117a9e35ff4c9deb425eb740d096158
angien90 <angelicanylander@hotmail.com>
2025-01-22 12:02:59 +0100

    feat(mark as done): Mark a single todo as done

--- todo/src/components/SingleTodo.vue
+++ todo/src/components/SingleTodo.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
-  defineProps({
-    todoText: { type: String, required: true },
-    complete: { type: Boolean, default: false },
-  });
+import { ref } from 'vue'; 
+
+const props = defineProps({
+  todoText: { type: String, required: true },
+  complete: { type: Boolean, default: false },
+});
+
+const isDone = ref(props.complete); 
+
+function toggleCompletedState() { 
+  isDone.value = !isDone.value; 
+} 
 </script>
 
 <template>
-  <div :class="{ completed: complete }">{{ todoText }}</div>
+  <div :class="{ completed: isDone }" class="grid grid-cols-2 gap-2">
+    <div>{{ todoText }}</div>
+    <div>
+      <button @click="toggleCompletedState">
+        <span v-if="isDone">Avmarkera som klar</span>
+        <span v-if="!isDone">Markera som klar</span>
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-  .completed {
-    @apply line-through; /* Eller någon styling i Sass */
-  }
+.completed {
+  text-decoration: line-through;
+}
 </style>
