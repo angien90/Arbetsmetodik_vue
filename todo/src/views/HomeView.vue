4c1217a6f766977180cbb362cabd7d60300b5dae
angien90 <angelicanylander@hotmail.com>
2025-01-22 11:53:35 +0100

    feat(add input & button): Add input field and button in HomeView

--- todo/src/views/HomeView.vue
+++ todo/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
 
+  const todos = ref<string[]>([]);
+  const todoName = ref('');
+
+  function addTodo() {
+    todos.value.push(todoName.value);
+  }
 </script>
 
 <template>
   <main>
-    Hem
+    <input type="text" v-model="todoName" @keyup.enter="addTodo" />
+    <button @click="addTodo">Lägg till todo</button>
   </main>
 </template>
