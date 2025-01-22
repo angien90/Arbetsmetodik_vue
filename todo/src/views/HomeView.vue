112a42fb0bfb2eeebaaf7e7a4cbaecb8f4f42c84
angien90 <angelicanylander@hotmail.com>
2025-01-22 11:56:16 +0100

    feat(write todo list): write a todo list on screen

--- todo/src/views/HomeView.vue
+++ todo/src/views/HomeView.vue
@@ -11,6 +11,16 @@
 
 <template>
   <main>
+    <div>
+      <ul v-if="todos.length > 0">
+        <li v-for="(todo, index) in todos" :key="index">
+          {{ todo }}
+        </li>
+      </ul>
+      
+      <p v-if="todos.length === 0">Du är klar med alla uppgifter!</p>
+    </div>
+    
     <input type="text" v-model="todoName" @keyup.enter="addTodo" />
     <button @click="addTodo">Lägg till todo</button>
   </main>
