a62fa201723787ec9c10c657e446c95214aa228b
angien90 <angelicanylander@hotmail.com>
2025-01-22 11:59:57 +0100

    feat(new component): New component for SingleTodo

---
+++ todo/src/components/SingleTodo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  defineProps({
+    todoText: { type: String, required: true },
+    complete: { type: Boolean, default: false },
+  });
+</script>
+
+<template>
+  <div :class="{ completed: complete }">{{ todoText }}</div>
+</template>
+
+<style scoped>
+  .completed {
+    @apply line-through; /* Eller någon styling i Sass */
+  }
+</style>
--- todo/src/views/HomeView.vue
+++ todo/src/views/HomeView.vue
@@ -1,26 +1,27 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
+import { ref } from 'vue';
+import SingleTodo from '@/components/SingleTodo.vue'; 
 
-  const todos = ref<string[]>([]);
-  const todoName = ref('');
+const todos = ref<string[]>([]);
 
-  function addTodo() {
-    todos.value.push(todoName.value);
-  }
+const todoName = ref('');
+
+function addTodo() {
+  todos.value.push(todoName.value);
+}
 </script>
 
 <template>
   <main>
-    <div>
-      <ul v-if="todos.length > 0">
-        <li v-for="(todo, index) in todos" :key="index">
-          {{ todo }}
-        </li>
-      </ul>
-      
+    <div class="my-8">
+
+      <div v-if="todos.length > 0" v-for="(todo, index) in todos" :key="index">
+        <SingleTodo :todo-text="todo" />
+      </div>
+
       <p v-if="todos.length === 0">Du är klar med alla uppgifter!</p>
+
     </div>
-    
     <input type="text" v-model="todoName" @keyup.enter="addTodo" />
     <button @click="addTodo">Lägg till todo</button>
   </main>
