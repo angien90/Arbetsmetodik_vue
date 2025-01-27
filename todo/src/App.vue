c952d24e09db9a12cdec821fb145b3320953c8a3
angien90 <angelicanylander@hotmail.com>
2025-01-27 09:39:20 +0100

    fix(name of sites): Changed name of sites in navigation bar

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -5,14 +5,37 @@ import { RouterLink, RouterView } from 'vue-router';
 <template>
   <header>
     <div class="wrapper">
-      
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/todo-list">Your Todo-list</RouterLink>
+        <RouterLink to="/">Hem</RouterLink>
+        <RouterLink to="/about">Om appen</RouterLink>
+        <RouterLink to="/todo-list">Avklarade</RouterLink>
       </nav>
     </div>
   </header>
 
   <RouterView />
 </template>
+
+<style scoped>
+  nav {
+    display: flex;
+    justify-content: center; 
+    align-items: center;
+    width: 100%;
+    margin-bottom: 30px;
+    color: black;
+    font-weight: bold;
+  }
+
+  nav a {
+    margin: 10px 20px; 
+    color: black;
+    text-decoration: none; 
+    transition: color 0.3s ease; 
+  }
+
+  nav a:hover {
+    font-size: 1.2rem;
+   text-decoration: underline;
+  }
+</style> 
